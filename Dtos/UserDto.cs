b4119f3daaaff59abd0998a56720ee44d4215ade
Tomislav Calusic <TomoCalu@gmail.com>
2019-09-07 17:10:22 +0200

    Removed Email anotation from UserDto

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -8,7 +8,6 @@ namespace WalletIO.Dtos
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        [EmailAddress]
         public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
